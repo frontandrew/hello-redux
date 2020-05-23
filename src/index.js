4f4a6b73c966c2c47d3aaab973ae0fcea9bd56a2
Andrew Bubnov <jackblack.nt@ya.ru>
2020-05-23 22:55:08 +1100

    Added Redux. Created store.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
 console.log('Hello Redux');
 
@@ -7,35 +6,43 @@ console.log('Hello Redux');
  * как нужно это state изменить - action.
  * 
  * Reduser возвращает новый state.
+ * 
+ * Если state undefind нужно вернуть initialState, 
+ * или state по умолчанию - в данном случае 0.
 */
 const reduser = (state = 0, action) => {
 
-  // в зависимости от аction reduser возвращает измененный state
+  /**В зависимости от аction reduser возвращает измененный state */
   switch (action.type) {
     case 'INC':
       return state + 1;
 
     case 'DEC':
       return state - 1;
-
+    /**Если action неизвестен, вернуть текущий state без изменений */
     default:
       return state;
   }
 };
 
-/** { type: 'INC' } - это action - обычный JS обьект,
+/**{ type: 'INC' } - это action - обычный JS обьект,
 * обязательно имеет поле type (тип действия, которое
 * нужно совершить над state)
 */
 
-//первая инициализация state
-let state = reduser(undefined, {})
+/**Инициализация store. Инициализирует state. */
+const store = createStore(reduser);
 
-state = reduser(state, { type: 'INC' });
-console.log(state);
+/**Subscribe выполняется после каждого изменения state */
+store.subscribe(() => {
+  console.log(store.getState())
+})
 
-state = reduser(state, { type: 'INC' });
-console.log(state);
-
-state = reduser(state, { type: 'DEC' });
-console.log(state);
+/**Dispatch принимает action и передает его в Reduser
+ * вместе со State. Reduser в зависимости от action изменяет state.
+ * Таким образом Dispatch приводит к изменению state.
+ */
+store.dispatch({ type: 'INC' })
+store.dispatch({ type: 'INC' })
+store.dispatch({ type: 'INC' })
+store.dispatch({ type: 'DEC' })
