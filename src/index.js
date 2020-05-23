3c98f457b25e34b2a2c58a0bc0a131f299b648ca
Andrew Bubnov <jackblack.nt@ya.ru>
2020-05-23 20:52:48 +1100

    Coded reduser func

--- src/index.js
+++ src/index.js
@@ -2,3 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 console.log('Hello Redux');
+
+/**Reduser получает на вход состояние state и указание
+ * как нужно это state изменить - action.
+ * 
+ * Reduser возвращает новый state.
+*/
+const reduser = (state = 0, action) => {
+
+  // в зависимости от аction reduser возвращает измененный state
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+
+    case 'DEC':
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+/** { type: 'INC' } - это action - обычный JS обьект,
+* обязательно имеет поле type (тип действия, которое
+* нужно совершить над state)
+*/
+
+//первая инициализация state
+let state = reduser(undefined, {})
+
+state = reduser(state, { type: 'INC' });
+console.log(state);
+
+state = reduser(state, { type: 'INC' });
+console.log(state);
+
+state = reduser(state, { type: 'DEC' });
+console.log(state);
