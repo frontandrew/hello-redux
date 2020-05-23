5779e0a9ed781ace053aeffa8d9a3dcf102de19e
Andrew Bubnov <jackblack.nt@ya.ru>
2020-05-23 17:44:25 +1100

    End of lesson 128

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, bindActionCreators } from 'redux';
+
+import reduser from './reduser';
+import * as actions from './actions';
+
+const store = createStore(reduser);
+const { dispatch } = store;
+
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+const renderCount = () => {
+  document
+    .getElementById('counter')
+    .textContent = store.getState()
+}
+
+document
+  .getElementById('inc')
+  .addEventListener('click', () => inc());
+
+document
+  .getElementById('dec')
+  .addEventListener('click', () => dec());
+
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    const count = Math.floor(Math.random() * 10);
+    rnd(count);
+  });
+
+store.subscribe(renderCount);
